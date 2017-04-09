ad886608686fb4afd4e237ebd0025869d7385cb4
Umut Seven <umutseven92@gmail.com>
2017-04-09 22:22:07 +0300

    Initial commit

--- graph_class.cpp
+++ graph_class.cpp
@@ -1,10 +1,3 @@
-//============================================================================
-// Name        : graph_class.cpp
-// Author      : 
-// Version     :
-// Copyright   : 
-// Description : Hello World in C++, Ansi-style
-//============================================================================
 
 #include <iostream>     // std::cout
 #include <fstream>      // std::ifstream
@@ -14,83 +7,102 @@
 
 using namespace std;
 
-void PrintList(vector< list<int> >& lst) {
-	  for (int i=0; i<lst.size(); i++) {
-		  cout << i << ": ";
-		  for (list<int>::iterator ip=lst[i].begin(); ip!=lst[i].end(); ip++) {
-			  cout << *ip << " ";
-		  }
-		  cout << endl;
-	  }
+void PrintList(vector<list < int> > &lst) {
+    for (int i = 0; i < lst.size(); i++) {
+        cout << i << ": ";
+        for (list<int>::iterator ip = lst[i].begin(); ip != lst[i].end(); ip++) {
+            cout << *ip << " ";
+        }
+        cout << endl;
+    }
 }
 
-
 class Graph {
-	int numNodes;
-	vector< list<int> > outlist;
-	vector< list<int> >  inlist;
+    int numNodes;
+    vector<list < int> >
+    outlist;
+    vector<list < int> >
+    inlist;
 public:
-	Graph(int N) {
-		numNodes = N;
-		outlist.resize(N);
-		inlist.resize(N);
-	}
+    Graph(int N) {
+        numNodes = N;
+        outlist.resize(N);
+        inlist.resize(N);
+    }
 
-	Graph(string fn) {
-		ReadFromFile(fn);
-	}
+    Graph(string fn) {
+        ReadFromFile(fn);
+    }
 
 
-	void Print() {
-		cout << "Outlist:" << endl;
-		PrintList(outlist);
-		cout << "Inlist:" << endl;
-		PrintList(inlist);
-	}
+    void Print() {
+        cout << "Outlist:" << endl;
+        PrintList(outlist);
+        cout << "Inlist:" << endl;
+        PrintList(inlist);
+    }
 
-	int InDegree(int vertex) {
+    int InDegree(int vertex) {
+        inlist[vertex].size();
+    }
 
-	}
-	
-	int OutDegree(int vertex) {
+    int OutDegree(int vertex) {
+        outlist[vertex].size();
+    }
 
-	}
-	
-	bool isNeighborDownStream(int neigh, int source) {
-	}
+    bool isNeighborDownStream(int neigh, int source) {
+        bool result = false;
 
-	bool isNeighborUpStream(int neigh, int source) {
-	}
+        for (list<int>::iterator ip = inlist[source].begin(); ip != inlist[source].end(); ip++) {
+            if (*ip == neigh) {
+                result = true;
+            }
+        }
+        return result;
+    }
 
-	bool isReachable(int target, int source) {
-	}
-	
-	void addEdge(int s, int t) {
-	}
-	
-	
-	
-	void ReadFromFile(string fn) {
-		ifstream ifs;
-		int s, t;
+    bool isNeighborUpStream(int neigh, int source) {
+        bool result = false;
 
-		ifs.open (fn, std::ifstream::in);
-		ifs >> numNodes;
-		inlist.resize(numNodes);
-		outlist.resize(numNodes);
+        for (list<int>::iterator ip = outlist[source].begin(); ip != outlist[source].end(); ip++) {
+            if (*ip == neigh) {
+                result =  true;
+            }
+        }
+        return result;
+    }
 
-		 ifs >> s >> t;
-		 while (ifs.good()) {
-			outlist[s].push_back(t);
-			inlist[t].push_back(s);
-			ifs >> s >> t;
-		 }
-		 ifs.close();
-	}
+    bool isReachable(int target, int source) {
+    }
+
+    void addEdge(int s, int t) {
+        outlist[s].push_back(t);
+        inlist[t].push_back(s);
+    }
+
+
+    void ReadFromFile(string fn) {
+        ifstream ifs;
+        int s, t;
+
+        ifs.open(fn, std::ifstream::in);
+        ifs >> numNodes;
+        inlist.resize(numNodes);
+        outlist.resize(numNodes);
+
+        ifs >> s >> t;
+        while (ifs.good()) {
+            outlist[s].push_back(t);
+            inlist[t].push_back(s);
+            ifs >> s >> t;
+        }
+        ifs.close();
+    }
 };
 
-int main () {
-	Graph g("/Users/cemgil/Downloads/data.txt");
-	g.Print();
-	return 0;
+int main() {
+    Graph g("C:\\Users\\Umut Seven\\Downloads\\data.txt");
+    g.Print();
+
+    return 0;
 }
